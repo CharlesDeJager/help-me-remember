1a378a1eabcbd51b7090cc315532c3687163e79a
Charles de Jager <62996317+CharlesDeJager@users.noreply.github.com>
2021-08-13 16:18:32 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'help-me-remember',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
